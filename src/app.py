b60b8e12bd21f09ad5eff4780686916740f232ca
Sharafat Ali <sharafatali0147@gmail.com>
2024-02-09 19:45:13 +0500

    APScheduler implemented

--- src/app.py
+++ src/app.py
@@ -5,6 +5,7 @@ from fastapi import FastAPI, Request
 from fastapi.exceptions import RequestValidationError
 from fastapi.responses import Response
 from fastapi_injector import InjectorMiddleware, attach_injector
+from apscheduler.schedulers.asyncio import AsyncIOScheduler
 from fastapi.middleware.cors import CORSMiddleware
 from injector import Injector
 from src.core.di import CoreModule
@@ -24,17 +25,27 @@ from src.user.di import UserModule
 log = logging.getLogger(__name__)
 
 injector = Injector([CoreModule(), UserModule()])
+scheduler = AsyncIOScheduler()
 
 
 @asynccontextmanager
 async def lifespan(app: FastAPI) -> AsyncGenerator[None, None]:
-    # scheduler.start()
     log.debug("==============starting app==================")
+    scheduler.start()
 
     yield
     log.debug("================ending app==================")
-    # scheduler.shutdown()
+    scheduler.shutdown()
 
+
+async def scheduled_reconcile() -> None:
+    log.debug("================calling every minutes==================")
+    # use_case = injector.get(ReconcileState)
+    # handler = injector.get(ReconcileState.Handler)
+    # await reconcile_loop(use_case, handler)
+
+
+scheduler.add_job(scheduled_reconcile, "interval", minutes=5)
 
 app = FastAPI(
     title="FastAPI Boilerplate API",
